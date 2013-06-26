4aefb51b95e41ff30ea4174eeb434817ab06bb11
Tomasz Pewiński <pewniak747@gmail.com>
2013-06-26 10:31:12 +0200

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+gem 'hipbot', github: 'pewniak747/hipbot'
+gem 'hipbot-plugins', github: 'netguru/hipbot-plugins'
---
+++ bot.rb
@@ -0,0 +1,23 @@
+class MyHipbot < Hipbot::Bot
+  configure do |c|
+    c.name      = ENV['HIPBOT_NAME']
+    c.jid       = ENV['HIPBOT_JID']
+    c.password  = ENV['HIPBOT_PASSWORD']
+    # optional configuration
+    # c.plugins   = [ PluginClass ]
+    # c.teams     = { vip: ['John', 'Mike'] }
+    # c.rooms     = { project_rooms: ['Project 1', 'Project 2'] }
+  end
+
+  desc 'this is a simple response'
+  on(/hello hipbot/) do
+    reply('hello human')
+  end
+
+  desc 'this is a response with arguments'
+  on(/my name is (\w+) (\w+)/) do |first_name, last_name|
+    reply("nice to meet you, #{first_name} #{last_name}!")
+  end
+end
+
+MyHipbot.start!
