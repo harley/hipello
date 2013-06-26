ed84a90a372517f743261a3f00254181464acb23
Tomasz Pewiński <pewniak747@gmail.com>
2013-06-26 12:27:56 +0200

    correct way to load plugins

--- bot.rb
+++ bot.rb
@@ -1,12 +1,14 @@
 require 'hipbot'
-require 'hipbot-plugins/human'
+# require 'hipbot-plugins/human'
+# require 'hipbot-plugins/github'
+# require 'hipbot-plugins/google'
+# check out more plugins on https://github.com/netguru/hipbot-plugins
 
 class MyHipbot < Hipbot::Bot
   configure do |c|
     c.jid       = ENV['HIPBOT_JID']
     c.password  = ENV['HIPBOT_PASSWORD']
     # optional configuration
-    c.plugins   = [ Human ]
     # c.teams     = { vip: ['John', 'Mike'] }
     # c.rooms     = { project_rooms: ['Project 1', 'Project 2'] }
   end
