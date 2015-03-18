d75ced09a94e0917c39232805ca172a02f012dd6
Harley Trung <harley@socialsci.com>
2015-03-18 23:13:04 +0700

    support HIPCHAT_TO_TRELLO_USERNAMES config var

--- lib/hipello/trello_handle.rb
+++ lib/hipello/trello_handle.rb
@@ -2,15 +2,18 @@ require_relative 'connectors'
 
 module Hipello
   class TrelloHandle
-    attr_reader :boards, :board_lists, :board_inbox, :errors, :board_members
-    attr_reader :current_card, :current_board, :current_list, :last_message
+    attr_reader :boards, :board_lists, :board_inbox, :board_members, :username_mapping
+    attr_reader :current_card, :current_board, :current_list, :last_message, :errors
     include Connectors
     include Singleton
 
     def initialize
       connect_to_trello
       load_boards
+      load_username_mapping
       @errors = []
+      # puts "\n\n\n[INSPECT] USERNAMES: #{username_mapping.inspect}"
+      # puts "\n\n\n[INSPECT] BOARD_MEMBERS: #{board_members.inspect}"
     end
 
     def load_boards
@@ -29,11 +32,23 @@ module Hipello
           @board_lists[tag] = board.lists
           @board_inbox[tag] = find_inbox_in(board.lists)
           board.members.each do |member|
-            @board_members[member.username] = member
+            # normalize by downsizing
+            @board_members[member.username.downcase] = member
           end
         end
       end
       @boards
+    end
+
+    def load_username_mapping
+      @username_mapping = {}
+      if raw = ENV['HIPCHAT_TO_TRELLO_USERNAMES']
+        raw.split(';').each do |s|
+          hipchat_u, trello_u = *s.split(':')
+          @username_mapping[hipchat_u] = trello_u
+        end
+      end
+      @username_mapping
     end
 
     def valid_tags
@@ -44,11 +59,23 @@ module Hipello
       lists.find {|e| e.name.match(/inbox/i)} || lists[0]
     end
 
+    def locate_member(hipchat_username)
+      if member = member_in_board(hipchat_username)
+        member
+      elsif trello_username = username_mapping[hipchat_username]
+        member_in_board(trello_username)
+      end
+    end
+
+    def member_in_board(username)
+        board_members[username.downcase]
+    end
+
     def create_card_in(mentions: [], hashtag:, sender:, title:, description: '')
       description << "\n-- asked by #{sender}"
-      members = mentions.map{|u| board_members[u]}.compact.uniq
-      # puts "[INSPECT] MENTIONS: #{mentions.inspect}"
-      # puts "[INSPECT] MEMBERS: #{members.inspect}"
+      members = mentions.uniq.map{|u| locate_member(u)}.compact
+      # puts "\n\n\n[INSPECT] MENTIONS: #{mentions.inspect}"
+      # puts "\n\n\n[INSPECT] LOCATED MEMBERS: #{members.inspect}"
 
       card_options = {name: title, desc: description}
       card_options.merge! member_ids: members.map(&:id)
