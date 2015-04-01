c55fc2e0f5ca16c1138ba420415496cad8f1ede1
Harley Trung <harley@socialsci.com>
2015-04-01 16:28:22 +0700

    ignore number only hashtag

--- lib/hipello.rb
+++ lib/hipello.rb
@@ -9,18 +9,20 @@ require_relative 'hipello/message_parser'
 module Hipello
   class MyHipbot < Hipbot::Bot
     on(/(.*)#(\w+)(.*)/, global: true) do |beforetag, hashtag, aftertag|
-      text = beforetag + aftertag
-      begin
-        @trello = MyHipbot.ask_trello(room, sender, text, hashtag)
-        if @trello.valid?
-          reply("added \"#{@trello.current_card.name[0..5]}…\" to [#{@trello.current_list.name}] in {#{@trello.current_board.name}} #{@trello.current_card.short_url}")
-        else
-          reply(@trello.display_errors)
+      # ignore number-only hashtags because they are usually github issue numbers
+      unless hashtag.match(/^\d+$/)
+        text = beforetag + aftertag
+        begin
+          @trello = MyHipbot.ask_trello(room, sender, text, hashtag)
+          if @trello.valid?
+            reply("added \"#{@trello.current_card.name[0..5]}…\" to [#{@trello.current_list.name}] in {#{@trello.current_board.name}} #{@trello.current_card.short_url}")
+          else
+            reply(@trello.display_errors)
+          end
+        rescue Exception => e
+          reply("ERROR: #{e}")
+          raise(e)
         end
-        # reply "\n\n\nROOM: #{room.inspect}"
-      rescue Exception => e
-        reply("ERROR: #{e}")
-        raise(e)
       end
     end
 
